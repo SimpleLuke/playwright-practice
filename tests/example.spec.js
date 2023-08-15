9a4f041eb15b47f6e8b0bd1e3d2a8eb2ad0fdeb6
Luke Lai <lukelai.dev@gmail.com>
2023-08-15 19:50:14 +0100

    Add beforeEach to visit playwright homepage before each test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,24 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe("Homepage", () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto("https://playwright.dev/");
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test("has title", async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test("get started link", async ({ page }) => {
+    // Click the get started link.
+    await page.getByRole("link", { name: "Get started" }).click();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    // Expects page to have a heading with the name of Installation.
+    await expect(
+      page.getByRole("heading", { name: "Installation" })
+    ).toBeVisible();
+  });
 });
